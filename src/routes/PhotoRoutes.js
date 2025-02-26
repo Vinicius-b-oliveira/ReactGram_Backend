a66d7686bf8d13ce3339391d441c86c1b0260cbb
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-02-26 14:38:38 -0300

    Finalizando função de criar fotos

--- src/routes/PhotoRoutes.js
+++ src/routes/PhotoRoutes.js
@@ -1,13 +1,23 @@
 import express from "express";
 
 // Controller
+import { insertPhoto } from "../controllers/PhotoController.js";
 
 // Middlewares
-import { photoIsertValidation } from "../middlewares/photoValidation";
-import authGuard from "../middlewares/authGuard";
-import validate from "../middlewares/handleValidation";
+import { photoIsertValidation } from "../middlewares/photoValidation.js";
+import authGuard from "../middlewares/authGuard.js";
+import validate from "../middlewares/handleValidation.js";
+import imageUpload from "../middlewares/imageUpload.js";
 
 // Routes
 const router = express.Router();
+router.post(
+    "/",
+    authGuard,
+    imageUpload.single("image"),
+    photoIsertValidation(),
+    validate,
+    insertPhoto
+);
 
 export default router;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "./UserRoutes.js";
+import photoRoutes from "./PhotoRoutes.js";
 
 const router = express.Router();
 
 router.use("/api/users", userRoutes);
+router.use("/api/photos", photoRoutes);
 
 // Rota de teste
 router.get("/", (req, res) => {
