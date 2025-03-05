e07f226bfedd89dd4323820aa336db5e67da729f
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-03-05 14:17:52 -0300

    Corrigindo problema com a validação da confirmação de senha

--- src/middlewares/userValidations.js
+++ src/middlewares/userValidations.js
@@ -20,7 +20,7 @@ const userCreateValidation = () => {
             .isLength({ min: 5 })
             .withMessage("A senha precisa ter no mínimo 5 caracteres"),
 
-        body("confirmpassword")
+        body("confirmPassword")
             .isString()
             .withMessage("A confirmação de senha é obrigatória")
             .custom((value, { req }) => {
